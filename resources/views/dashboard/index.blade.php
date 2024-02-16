9f46ec07bad0722394e33c29e60ec5b81460a004
fadli zainul ihsani <fadlizainulihsani@gmail.com>
2024-02-16 10:08:24 +0800

    merubah css primarynya

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,11 +7,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link href="{{asset("admin/img/logo/logo.png")}}" rel="icon">
+  <link href="{{asset('admin/img/logo/logo.png')}}" rel="icon">
   <title>RuangAdmin - Dashboard</title>
-  <link href="{{asset("admin/vendor/fontawesome-free/css/all.min.css")}}" rel="stylesheet" type="text/css">
-  <link href="{{asset("admin/vendor/bootstrap/css/bootstrap.min.css")}}" rel="stylesheet" type="text/css">
-  <link href="{{asset("admin/css/ruang-admin.css")}}" rel="stylesheet">
+  <link href="{{asset('admin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{asset('admin/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{asset('admin/css/ruang-admin.css')}}" rel="stylesheet">
 </head>
 
 <body id="page-top">
@@ -20,7 +20,7 @@
     <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon">
-          <img src="{{asset("admin/img/logo/awas-banjir.png")}}">
+          <img src="{{asset('admin/img/logo/awas-banjir.png')}}">
         </div>
         <div class="sidebar-brand-text mx-3">Admin</div>
       </a>
@@ -35,8 +35,7 @@
         Features
       </div>
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap"
-          aria-expanded="true" aria-controls="collapseBootstrap">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap" aria-expanded="true" aria-controls="collapseBootstrap">
           <i class="far fa-fw fa-window-maximize"></i>
           <span>Bootstrap UI</span>
         </a>
@@ -53,8 +52,7 @@
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm" aria-expanded="true"
-          aria-controls="collapseForm">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm" aria-expanded="true" aria-controls="collapseForm">
           <i class="fab fa-fw fa-wpforms"></i>
           <span>Forms</span>
         </a>
@@ -67,8 +65,7 @@
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTable" aria-expanded="true"
-          aria-controls="collapseTable">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTable" aria-expanded="true" aria-controls="collapseTable">
           <i class="fas fa-fw fa-table"></i>
           <span>Tables</span>
         </a>
@@ -91,8 +88,7 @@
         Examples
       </div>
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePage" aria-expanded="true"
-          aria-controls="collapsePage">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePage" aria-expanded="true" aria-controls="collapsePage">
           <i class="fas fa-fw fa-columns"></i>
           <span>Pages</span>
         </a>
@@ -125,9 +121,8 @@
           </button>
           <ul class="navbar-nav ml-auto">
             <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                <img class="img-profile rounded-circle" src="{{asset("admin/img/boy.png")}}" style="max-width: 60px">
+              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img class="img-profile rounded-circle" src="{{asset('admin/img/boy.png')}}" style="max-width: 60px">
                 <span class="ml-2 d-none d-lg-inline text-white small">Maman Ketoprak</span>
               </a>
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -248,14 +243,13 @@
             </div>
 
             <!-- Area Chart -->
-          
+
             <!-- Invoice Example -->
             <div class="col-xl-8 col-lg-7 mb-4">
               <div class="card">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 class="m-0 font-weight-bold text-primary">Invoice</h6>
-                  <a class="m-0 float-right btn btn-danger btn-sm" href="#">View More <i
-                      class="fas fa-chevron-right"></i></a>
+                  <a class="m-0 float-right btn btn-danger btn-sm" href="#">View More <i class="fas fa-chevron-right"></i></a>
                 </div>
                 <div class="table-responsive">
                   <table class="table align-items-center table-flush">
@@ -347,8 +341,7 @@
                     </a>
                   </div>
                   <div class="card-footer text-center">
-                    <a class="m-0 small text-primary card-link" href="#">View More <i
-                        class="fas fa-chevron-right"></i></a>
+                    <a class="m-0 small text-primary card-link" href="#">View More <i class="fas fa-chevron-right"></i></a>
                   </div>
                 </div>
               </div>
@@ -358,10 +351,9 @@
 
           <!-- End Content -->
 
-          
+
           <!-- Modal Logout -->
-          <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout"
-            aria-hidden="true">
+          <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout" aria-hidden="true">
             <div class="modal-dialog" role="document">
               <div class="modal-content">
                 <div class="modal-header">
@@ -386,11 +378,13 @@
       </div>
       <!-- Footer -->
       <footer class="sticky-footer bg-white">
-      
+
 
         <div class="container my-auto py-2">
           <div class="copyright text-center my-auto">
-            <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - distributed by
+            <span>copyright &copy; <script>
+                document.write(new Date().getFullYear());
+              </script> - distributed by
               <b><a href="https://themewagon.com/" target="_blank">themewagon</a></b>
             </span>
           </div>
@@ -405,12 +399,12 @@
     <i class="fas fa-angle-up"></i>
   </a>
 
-  <script src="{{asset("admin/vendor/jquery/jquery.min.js")}}"></script>
-  <script src="{{asset("admin/vendor/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
-  <script src="{{asset("admin/vendor/jquery-easing/jquery.easing.min.js")}}"></script>
-  <script src="{{asset("admin/js/ruang-admin.min.js")}}"></script>
-  <script src="{{asset("admin/vendor/chart.js/Chart.min.js")}}"></script>
-  <script src="{{asset("admin/js/demo/chart-area-demo.js")}}"></script>  
+  <script src="{{asset('admin/vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('admin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+  <script src="{{asset('admin/js/ruang-admin.min.js')}}"></script>
+  <script src="{{asset('admin/vendor/chart.js/Chart.min.js')}}"></script>
+  <script src="{{asset('admin/js/demo/chart-area-demo.js')}}"></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
+Route::get('/', [DashboardController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard.index');
